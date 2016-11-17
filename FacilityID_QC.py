e99715786e50fb8b8309b090acf8d22cb6841178
jzhengli <lizhengriverside@gmail.com>
2016-11-17 16:43:50 -0500

    minor changes, add description

--- FacilityID_QC.py
+++ FacilityID_QC.py
@@ -1,3 +1,4 @@
+## Script for QCing FacilityID of all major features, result being logged in a log file in same directory
 import arcpy, logging, os, sys, time
 
 arcpy.env.overwriteOutput = True
@@ -15,7 +16,7 @@ def countFeature(fc, lyr, whereClause=""):
 	return result
 
 
-def diagnoseField(fd, fc, prefix, field="FACILITYID"):
+def diagnoseField(fd, fc, prefix="", field="FACILITYID"):
 	featureDataset = fd
 	featureClass = fc
 	layerName = fc + "_lyr"
@@ -35,48 +36,59 @@ def diagnoseField(fd, fc, prefix, field="FACILITYID"):
 	logMessage("{0} out of {1} not null value do not have correct prefix '{2}' in FacilityID".format(wrongPrefix, notNull, idPrefix))
 	logMessage("...")
 
-logMessage("Start")
+
 ## Water
-dataset = "RPUD.WaterDistributionNetwork/"
-diagnoseField(dataset, "RPUD.wCasing", "WCAS")
-diagnoseField(dataset, "RPUD.wControlValve", "WCV")
-diagnoseField(dataset, "RPUD.wFitting", "WFIT")
-diagnoseField(dataset, "RPUD.wGravityMain", "WGM")
-diagnoseField(dataset, "RPUD.wHydrant", "WHYD")
-diagnoseField(dataset, "RPUD.wLateralLine", "WLAT")
-diagnoseField(dataset, "RPUD.wNetworkStructure", "WNS")
-diagnoseField(dataset, "RPUD.wPressureMain", "WMN")
-diagnoseField(dataset, "RPUD.wSamplingStation", "WSS")
-diagnoseField(dataset, "RPUD.wServiceConnection", "WSC")
-diagnoseField(dataset, "RPUD.wSystemValve", "WSV")
+def checkWater():
+	dataset = "RPUD.WaterDistributionNetwork/"
+	diagnoseField(dataset, "RPUD.wCasing", "WCAS")
+	diagnoseField(dataset, "RPUD.wControlValve", "WCV")
+	diagnoseField(dataset, "RPUD.wFitting", "WFIT")
+	diagnoseField(dataset, "RPUD.wGravityMain", "WGM")
+	diagnoseField(dataset, "RPUD.wHydrant", "WHYD")
+	diagnoseField(dataset, "RPUD.wLateralLine", "WLAT")
+	diagnoseField(dataset, "RPUD.wNetworkStructure", "WNS")
+	diagnoseField(dataset, "RPUD.wPressureMain", "WMN")
+	diagnoseField(dataset, "RPUD.wSamplingStation", "WSS")
+	diagnoseField(dataset, "RPUD.wServiceConnection", "WSC")
+	diagnoseField(dataset, "RPUD.wSystemValve", "WSV")
 
 ## Sewer
-dataset = "RPUD.SewerCollectionNetwork/"
-diagnoseField(dataset, "RPUD.ssAerial", "SAM")
-diagnoseField(dataset, "RPUD.ssCasing", "SCA")
-diagnoseField(dataset, "RPUD.ssCleanout", "SCO")
-diagnoseField(dataset, "RPUD.ssControlValve", "SCV")
-diagnoseField(dataset, "RPUD.ssFitting", "SF")
-diagnoseField(dataset, "RPUD.ssForceMain", "SFMN")
-diagnoseField(dataset, "RPUD.ssGravityMain", "SGMN")
-diagnoseField(dataset, "RPUD.ssGreaseTrap", "SGT")
-diagnoseField(dataset, "RPUD.ssLateralLine", "SLAT")
-diagnoseField(dataset, "RPUD.ssManhole", "SMH")
-diagnoseField(dataset, "RPUD.ssMeterStation", "SMS")
-diagnoseField(dataset, "RPUD.ssNetworkStructure", "SNS")
-diagnoseField(dataset, "RPUD.ssSystemValve", "SSV")
+def checkSewer():
+	dataset = "RPUD.SewerCollectionNetwork/"
+	diagnoseField(dataset, "RPUD.ssAerial", "SAM")
+	diagnoseField(dataset, "RPUD.ssCasing", "SCA")
+	diagnoseField(dataset, "RPUD.ssCleanout", "SCO")
+	diagnoseField(dataset, "RPUD.ssControlValve", "SCV")
+	diagnoseField(dataset, "RPUD.ssFitting", "SF")
+	diagnoseField(dataset, "RPUD.ssForceMain", "SFMN")
+	diagnoseField(dataset, "RPUD.ssGravityMain", "SGMN")
+	diagnoseField(dataset, "RPUD.ssGreaseTrap", "SGT")
+	diagnoseField(dataset, "RPUD.ssLateralLine", "SLAT")
+	diagnoseField(dataset, "RPUD.ssManhole", "SMH")
+	diagnoseField(dataset, "RPUD.ssMeterStation", "SMS")
+	diagnoseField(dataset, "RPUD.ssNetworkStructure", "SNS")
+	diagnoseField(dataset, "RPUD.ssSystemValve", "SSV")
 
 ## Reuse
-dataset = "RPUD.ReclaimedWaterDistributionNetwork/"
-diagnoseField(dataset, "RPUD.rCasing", "RCAS")
-diagnoseField(dataset, "RPUD.rControlValve", "RCV")
-diagnoseField(dataset, "RPUD.rFitting", "RFIT")
-diagnoseField(dataset, "RPUD.rHydrant", "RHYD")
-diagnoseField(dataset, "RPUD.rLateralLine", "RLAT")
-diagnoseField(dataset, "RPUD.rNetworkStructure", "RNS")
-diagnoseField(dataset, "RPUD.rPressureMain", "RMN")
-diagnoseField(dataset, "RPUD.rSamplingStation", "RSS")
-diagnoseField(dataset, "RPUD.rServiceConnection", "RSC")
-diagnoseField(dataset, "RPUD.rSystemValve", "RSV")
+def checkReuse():
+	dataset = "RPUD.ReclaimedWaterDistributionNetwork/"
+	diagnoseField(dataset, "RPUD.rCasing", "RCAS")
+	diagnoseField(dataset, "RPUD.rControlValve", "RCV")
+	diagnoseField(dataset, "RPUD.rFitting", "RFIT")
+	diagnoseField(dataset, "RPUD.rHydrant", "RHYD")
+	diagnoseField(dataset, "RPUD.rLateralLine", "RLAT")
+	diagnoseField(dataset, "RPUD.rNetworkStructure", "RNS")
+	diagnoseField(dataset, "RPUD.rPressureMain", "RMN")
+	diagnoseField(dataset, "RPUD.rSamplingStation", "RSS")
+	diagnoseField(dataset, "RPUD.rServiceConnection", "RSC")
+	diagnoseField(dataset, "RPUD.rSystemValve", "RSV")
 
-logMessage("End")
+def main()
+	logMessage("Start")
+	checkWater()
+	checkSewer()
+	checkReuse()
+	logMessage("End")
+
+if __name__ == "__main__":
+    main()
